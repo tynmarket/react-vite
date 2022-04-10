d67e9a806392e23a79a4e54ff6c14151e1cf4bc5
tynmarket <tynmarket@gmail.com>
2022-04-10 17:59:17 +0900

    活動時間のスタイル追加

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,4 +1,5 @@
 import logo from './card-img.png'
+import clock from './clock.svg'
 import './Card.css'
 
 export default function Card() {
@@ -9,8 +10,11 @@ export default function Card() {
         <header>
           <h2 class="card-title">【Gaia】絶竜詩 vc無し 活動日週5以上固定メンバー2名募集@ST or メレー、レンジ</h2>
         </header>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
+        <div class="card-content">
+          <h3 class="flex activity-time__header">
+            <img src={ clock } />
+            活動時間
+          </h3>
         </div>
       </div>
     </div>
