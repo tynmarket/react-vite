c5f554de62d6e7adcb23394eb46084b9041ed663
tynmarket <tynmarket@gmail.com>
2022-04-10 17:15:14 +0900

    headerのスタイル追加

--- src/Card.tsx
+++ src/Card.tsx
@@ -6,8 +6,9 @@ export default function Card() {
     <div class="card w-96 bg-base-100 shadow-xl" style={{ width: "270px", margin: "auto" }}>
       <figure><img src={ logo } alt="Shoes" /></figure>
       <div class="card-body" style={{ height: "424px", padding: "18px 14px 28px 16px" }}>
-        <h2 class="card-title">【Gaia】絶竜詩 vc無し 活動日週5以上固定メンバー2名募集@ST or メレー、レンジ</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <header>
+          <h2 class="card-title">【Gaia】絶竜詩 vc無し 活動日週5以上固定メンバー2名募集@ST or メレー、レンジ</h2>
+        </header>
         <div class="card-actions justify-end">
           <button class="btn btn-primary">Buy Now</button>
         </div>
