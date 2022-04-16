f8ec13ee59777dd6cf2bdca603058f62e837fc8d
tynmarket <tynmarket@gmail.com>
2022-04-16 22:50:05 +0900

    ホームワールド追加

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,4 +1,5 @@
 import face from './face.svg'
+import home from './home.svg'
 import './Layout.css'
 
 export default function Layout() {
@@ -7,13 +8,19 @@ export default function Layout() {
       <div class="brand w-full"></div>
       <header class="header flex items-center w-full">
         <div class="header__inner flex justify-end mx-auto">
-          <div class="my-character flex justify-between items-center">
+          <div class="my-character flex justify-between items-center relative">
             <span class="bg-white rounded-full">
               <img src={ face } class="icon" />
             </span>
             <span class="profile flex flex-col">
               <span class="profile__name font-bold">Hikari Senshi</span>
               <span class="profile__free-company">Tosho Markets</span>
+            </span>
+            <span class="home-world flex items-center">
+              <span class="icon-wrapper rounded-full">
+                <img src={ home } class="icon" />
+              </span>
+              Ultima
             </span>
           </div>
         </div>
