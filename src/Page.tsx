92de8753ac70708a7146abe9ffad2f494351928a
tynmarket <tynmarket@gmail.com>
2022-04-17 05:29:09 +0900

    文言など微修正

--- src/Page.tsx
+++ src/Page.tsx
@@ -5,12 +5,13 @@ import './Page.css'
 export default function Page() {
   return (
     <div class="pages">
+      { /* Sticky */ }
       <div class="nav-bar flex justify-between w-full bg-primary">
         <div class="flex items-center">
           <img src={ party } class="icon-party" />
           <div class="flex flex-col">
             <span class="font-s font-bold">固定パーティー</span>
-            <span class="title font-bold">Tyn Marketの絶竜詩戦争 メンバー募集</span>
+            <span class="title font-bold">絶竜詩戦争 メンバー募集</span>
             <span class="period flex items-center font-medium">
               <img src={ calendar } class="icon-calendar" />
               募集期間: 2022/05/13 まで
@@ -24,6 +25,9 @@ export default function Page() {
           <span class="nav-item">募集主プロフィール</span>
           <span class="nav-item">コメント</span>
         </div>
+      </div>
+      <div class="">
+
       </div>
     </div>
   );
