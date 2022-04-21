9591d445023dd4ab25f27fe3b17aa34804761ddd
tynmarket <tynmarket@gmail.com>
2022-04-21 09:29:10 +0900

    カテゴリー追加

--- src/Pages.tsx
+++ src/Pages.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 import Card from "./Card";
 
+import party from './party.svg'
 import search from './search.svg'
 import './Pages.css'
 
@@ -10,7 +11,17 @@ export default function Expenses() {
     <main>
       <div class="pages">
         <div class="search flex justify-between">
-          <ul class="">aaa
+          <ul class="category flex items-center text-center font-bold">
+            <li class="all"></li>
+            <li class="fc"></li>
+            <li class="ls"></li>
+            <li class="party flex justify-center items-center">
+              <img src={ party } class="icon mr-8" />
+              <span>
+                固定パーティー
+                <span class="ml-4 font-11 font-normal">(3)</span>
+              </span>
+            </li>
           </ul>
           <span class="search-button flex items-center rounded-8 text-white font-bold">
             <span class="icon-wrapper flex items-center">
