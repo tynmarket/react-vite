5cd950e52e2eb0d1a05e3d6bb5de4e0f08d48b45
tynmarket <tynmarket@gmail.com>
2022-04-24 20:27:23 +0900

    ヘッダ表示

--- src/NewPage.tsx
+++ src/NewPage.tsx
@@ -1,8 +1,18 @@
+import party_w from './party-white.svg'
 import './NewPage.css'
 
 export default function NewPage() {
   return (
-    <div class="page">
+    <div class="new-page">
+      <div class="nav-bar flex justify-between w-full bg-primary">
+        <div class="flex items-center">
+          <img src={ party_w } class="icon-party" />
+          <div class="flex flex-col">
+            <span class="font-s font-bold">固定パーティー</span>
+            <span class="title font-bold">[募集ページ作成]</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
